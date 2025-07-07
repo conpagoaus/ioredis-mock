11966edbfc8b199ed966bf602c573fa92b1d2cff
Emanuele Tonello <emanuele@conpago.com.au>
2025-07-07 11:21:27 +1000

    refactor: test compatible with browser environment

--- test/integration/disconnect.js
+++ test/integration/disconnect.js
@@ -1,15 +1,17 @@
 import Redis from 'ioredis'
 
 describe('disconnect', () => {
-  it('should be available and emit "end" event', async () => {
+  it('should be available and emit "end" event', done => {
     const redis = new Redis({})
-    const events = []
-    redis.on('end', () => events.push('end'))
+    let eventEmitted = false
 
     expect(redis.disconnect()).toBe(undefined)
 
-    await new Promise(resolve => setImmediate(resolve));
+    redis.on('end', () => {
+      eventEmitted = true
 
-    expect(events).toEqual(['end'])
+      expect(eventEmitted).toEqual(true)
+      done()
+    })
   })
 })
